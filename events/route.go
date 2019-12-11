e4d361e47ec651fe22c71b31f915b95dadf12824
Wilson <heyw@yeefung.com>
2019-12-11 01:57:50 -0800

    topic 使用header传递type来区分话题的类型

--- events/route.go
+++ events/route.go
@@ -1,13 +1,15 @@
 package events
 
-// TopicStationInit ..
-const TopicStationInit = "go.micro.topic.map.station.init"
+const (
+	// TopicTypeInit ..
+	TopicTypeInit = "init"
+	// TopicTypeAdd ..
+	TopicTypeAdd = "add"
+	// TopicTypeDelete ..
+	TopicTypeDelete = "delete"
+	// TopicTypeUpdate ..
+	TopicTypeUpdate = "update"
+)
 
-// TopicStationUpdate ..
-const TopicStationUpdate = "go.micro.topic.map.station.update"
-
-// TopicStationDelete ..
-const TopicStationDelete = "go.micro.topic.map.station.delete"
-
-// TopicStationAdd ..
-const TopicStationAdd = "go.micro.topic.map.station.add"
+// TopicStation ..
+const TopicStation = "go.micro.topic.map.station"
