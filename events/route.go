dd716a0c9cbf1dd0cf22ebdeb9819f75b10ed999
Wilson <heyw@yeefung.com>
2019-12-11 00:02:20 -0800

    增加station增删改的订阅发布话题

---
+++ events/route.go
@@ -0,0 +1,13 @@
+package events
+
+// TopicStationInit ..
+const TopicStationInit = "go.micro.topic.map.station.init"
+
+// TopicStationUpdate ..
+const TopicStationUpdate = "go.micro.topic.map.station.update"
+
+// TopicStationDelete ..
+const TopicStationDelete = "go.micro.topic.map.station.delete"
+
+// TopicStationAdd ..
+const TopicStationAdd = "go.micro.topic.map.station.add"
