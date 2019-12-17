c3846dde8f5fadc339b728e60ee4fe6e1a7fc9bc
Wilson <heyw@yeefung.com>
2019-12-16 17:30:30 -0800

    增加段的显示和隐藏事件

--- events/route.go
+++ events/route.go
@@ -9,7 +9,17 @@ const (
 	TopicTypeDelete = "delete"
 	// TopicTypeUpdate ..
 	TopicTypeUpdate = "update"
+	// TopicTypeShow ..
+	TopicTypeShow = "show"
+	// TopicTypeHide ..
+	TopicTypeHide = "hide"
 )
 
 // TopicStation ..
 const TopicStation = "go.micro.topic.map.station"
+
+// TopicSegment ..
+const TopicSegment = "go.micro.topic.map.segment"
+
+// TopicPoint ..
+const TopicPoint = "go.micro.topic.map.point"
