d896052c71f7b0f9c99c230bc94d656fe525cf58
Wilson <heyw@yeefung.com>
2020-01-17 01:09:50 -0800

    modify agv info

--- events/agvinfo.go
+++ events/agvinfo.go
@@ -1,29 +1,5 @@
 package events
 
-const AgvInfoTopic = "go.micro.topic.agvinfo"
+// TopicAgv ..
+const TopicAgv = "go.micro.topic.agv"
 
-type VehicleType uint32
-
-const (
-	_ VehicleType = iota
-	VehicleTypeI
-	VehicleTypeII
-	VehicleTypeIII
-	VehicleTypeIV
-	VehicleTypeV
-	VehicleTypeVI
-	VehicleTypeVII
-	VehicleTypeVIII
-)
-
-type AgvInfo struct {
-	AgvID             int
-	Type              VehicleType
-	Point             int32
-	Segment           int32
-	X                 int32
-	Y                 int32
-	Angle             int32
-	QuantityOfBattery uint16
-	IsCharging        bool
-}
