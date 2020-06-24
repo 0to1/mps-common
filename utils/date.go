52ec4a36c55c78db081b08262f772a307ed56a01
Wilson <heyw@yeefung.com>
2020-06-24 12:36:40 +0800

    增加时间统一格式

--- utils/date.go
+++ utils/date.go
@@ -6,6 +6,9 @@ import (
 	"time"
 )
 
+// DateTimeFormat 时间转换格式
+const DateTimeFormat = "2006-01-02 15:04:05"
+
 // StrToIntMonth 字符串月份转整数月份
 func StrToIntMonth(month string) int {
 	var data = map[string]int{
